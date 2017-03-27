165fb51d94c5d577a6c84fac7bc1b7627e5c7e26
John Bellone <jbellone@bloomberg.net>
2017-03-27 08:52:31 -0400

    Update integration tests to run test default first.

--- test/fixtures/policies/default.rb
+++ test/fixtures/policies/default.rb
@@ -2,7 +2,7 @@ name 'default'
 default_source :community
 default_source :chef_repo, '..'
 cookbook 'gemrc', path: '../../..'
-run_list 'gemrc::default', 'test::default'
+run_list 'test::default'
 named_run_list :centos, 'sudo::default', 'yum::default', 'yum-epel::default', run_list
 named_run_list :debian, 'sudo::default', 'apt::default', run_list
 named_run_list :freebsd, 'freebsd::default', run_list
